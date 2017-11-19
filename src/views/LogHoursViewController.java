5e75b98f1a75b1515d52c05cf882dd2cab9b15fd
JaretWright <hkkA223>
2017-11-19 11:21:13 -0500

    Added lineChart and CSS changes

--- src/views/LogHoursViewController.java
+++ src/views/LogHoursViewController.java
@@ -3,12 +3,20 @@ package views;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -30,7 +38,14 @@ public class LogHoursViewController implements Initializable, ControllerClass {
     @FXML    private Label lastNameLabel;
     @FXML    private Label errMsgLabel;
     @FXML    private Button backButton;
+    
+    //objects used in the line chart
+    @FXML    private LineChart<?, ?> lineChart;
+    @FXML    private CategoryAxis monthAxis;
+    @FXML    private NumberAxis hoursAxis;
+             private XYChart.Series hoursLoggedSeries;
 
+   
     private Volunteer volunteer;
     
     /**
@@ -70,8 +85,82 @@ public class LogHoursViewController implements Initializable, ControllerClass {
         lastNameLabel.setText(volunteer.getLastName());
         datePicker.setValue(LocalDate.now());
         errMsgLabel.setText("");
+        
+        updateLineChart();
     }
     
+    /**
+     * The goal of this method is to update the line chart with the latest
+     * information stored in the database
+     */
+    public void updateLineChart()
+    {
+        //initialize the instance variables for the chart
+        hoursLoggedSeries = new XYChart.Series<>();
+        hoursLoggedSeries.setName(Integer.toString(LocalDate.now().getYear()));
+        lineChart.getData().clear();
+        
+        try{
+            populateSeriesFromDB();
+        }
+        catch (SQLException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        lineChart.getData().addAll(hoursLoggedSeries);
+    }
+    
+    /**
+     * This method will populate the hoursLoggedSeries with the latest info
+     * from the database
+     */
+    public void populateSeriesFromDB() throws SQLException
+    {
+        Connection conn=null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        
+        try{
+            //1.  Connect to the database
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/volunteer?useSSL=false", "student", "student");
+            
+            //2. create a String with the sql statement
+            String sql =    "SELECT MONTHNAME(dateWorked), SUM(hoursWorked) " +
+                            "FROM hoursworked " +
+                            "WHERE volunteerID=? AND YEAR(dateWorked)=? " +
+                            "GROUP BY MONTH(dateWorked);";
+            
+            //3. create the statement
+            statement = conn.prepareCall(sql);
+            
+            //4. bind the parameters
+            statement.setInt(1, volunteer.getVolunteerID());
+            statement.setInt(2, LocalDate.now().getYear());
+            
+            //5. execute the query
+            resultSet = statement.executeQuery();
+            
+            //6. loop over the result set and build the series
+            while (resultSet.next())
+            {
+                hoursLoggedSeries.getData().add(new XYChart.Data(resultSet.getString(1), resultSet.getInt(2)));
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            if (conn != null)
+                conn.close();
+            if (statement != null)
+                statement.close();
+            if (resultSet != null)
+                resultSet.close();
+        }
+    }
+        
     /**
      * This method will read/validate the inputs and store the information
      * in the hoursWorked table
@@ -79,7 +168,9 @@ public class LogHoursViewController implements Initializable, ControllerClass {
     public void saveButtonPushed(ActionEvent event)
     {
         try{
-            volunteer.logHours(datePicker.getValue(), (int) hoursWorkedSpinner.getValue());           
+            volunteer.logHours(datePicker.getValue(), (int) hoursWorkedSpinner.getValue()); 
+            errMsgLabel.setText("Hours logged");
+            updateLineChart();
         }
         catch (SQLException e)
         {
